9c09d26ef137c3f570fc5e3f951abfd92026fb1d
Jalilov <javohirjalilov777@gmail.com>
2022-11-30 17:31:14 +0500

    done

---
+++ Slicing_list01.py
@@ -0,0 +1,9 @@
+def main(numbers):
+    """
+    A list called numbers is given. Return the items in the odd position.
+    Args:
+        numbers(list): parameter
+    Returns:
+        list: return answer.
+    """
+    return
